dcfb2e3c59e0493c7f921418d0536c8de8def056
AlexSuRus <noknotar@gmail.com>
2023-09-25 22:21:44 +0300

    Final doc

--- marketplace/cart.py
+++ marketplace/cart.py
@@ -1,25 +1,28 @@
-import uuid
-import zipfile
-import shutil
-import tempfile
-from flask import (
+# Импортируем необходимые модули и функции
+import uuid  # Модуль для работы с уникальными идентификаторами
+import zipfile  # Модуль для работы с ZIP-архивами
+import shutil  # Модуль для работы с файлами и директориями
+import tempfile  # Модуль для работы с временными файлами и директориями
+from flask import (  # Модуль Flask для работы с веб-приложением
     Blueprint, flash, g, redirect, render_template, request, url_for, send_file, redirect, Flask
 )
-import csv
+import csv  # Модуль для работы с CSV-файлами
 
-from marketplace.db import get_db
+from marketplace.db import get_db  # Модуль для работы с базой данных
+# Декоратор функции для проверки наличия аутентификации
 from marketplace.auth import login_required
-from flask import Blueprint, g, flash, redirect, url_for
-from flask_mail import Mail
-from flask_mail import Message
-import os
+# Модуль Flask-Mail для отправки электронной почты
+from flask_mail import Mail, Message
+import os  # Модуль для работы с операционной системой
 
-
+# Создаем экземпляр Blueprint с именем 'cart'
 bp = Blueprint('cart', __name__)
+
+# Функция для добавления товара в корзину
 
 
 @bp.route('/add_cart/<int:item_id>', methods=['POST'])
-@login_required
+@login_required  # Декоратор для проверки наличия аутентификации
 def add_cart(item_id):
     db = get_db()
     # TODO
@@ -31,8 +34,9 @@ def add_cart(item_id):
         (g.user['id'], item_id, item_id)
     )
     db.commit()
-    # flash("Item successfully added to cart", 'success')
     return redirect(url_for('store.index'))
+
+# Функция для чтения CSV-файла
 
 
 def read_file(path):
@@ -42,14 +46,19 @@ def read_file(path):
         lines = [tuple(row) for row in reader]
     return (lines[0], lines[1:])
 
+# Функция для отображения таблицы из CSV-файла
+
 
 def table(path):
-    headers, rows = read_file(path)  # 'youtubers_df.csv')
+    headers, rows = read_file(path)  # Читаем данные из CSV-файла
+    # Отображаем таблицу в HTML-шаблоне
     return render_template('columns.html', headers=headers, rows=rows)
+
+# Функция для предварительного просмотра товара
 
 
 @bp.route('/preview/<int:item_id>', methods=['POST', 'GET'])
-@login_required
+@login_required  # Декоратор для проверки наличия аутентификации
 def preview(item_id):
     db = get_db()
     file_name = db.execute(
@@ -59,12 +68,13 @@ def preview(item_id):
 
     db.commit()
     path = f"static/files/{file_name['file_name']}"
-    # flash("Item successfully added to cart", 'success')
     return table(path)
+
+# Функция для оформления заказа
 
 
 @bp.route('/checkout', methods=['GET'])
-@login_required
+@login_required  # Декоратор для проверки наличия аутентификации
 def checkout():
     db = get_db()
     all = db.execute('Select * from cart').fetchall()
@@ -77,8 +87,9 @@ def checkout():
     return render_template('cart/checkout.html', cart_items=cart_items)
 
 
+# Функция для фильтрации товаров по автору набора данных
 @bp.route('/tag/<item_dataset_author>', methods=['GET'])
-@login_required
+@login_required  # Декоратор для проверки наличия аутентификации
 def tag(item_dataset_author):
     import re
     item_dataset_author = item_dataset_author.strip().lower()
@@ -97,14 +108,18 @@ def tag(item_dataset_author):
 
     return render_template('store/index.html', items=new_items)
 
+# Функция для удаления товара из корзины
+
 
 @bp.route('/delete/<cart_item_id>', methods=['POST'])
-@login_required
+@login_required  # Декоратор для проверки наличия аутентификации
 def delete_item(cart_item_id):
     db = get_db()
     db.execute('DELETE FROM cart WHERE cart_id = ?', [cart_item_id])
     db.commit()
     return redirect(url_for('cart.checkout'))
+
+# Функция для создания ZIP-архива
 
 
 def create_zip_archive(file_list, archive_name):
@@ -114,12 +129,13 @@ def create_zip_archive(file_list, archive_name):
                 os.getcwd(),
                 fr'marketplace\static\files\{file_path}'
             )
-            # file_name = os.path.basename(file_path)
             zip_file.write(file_name, file_path)
+
+# Функция для скачивания ZIP-архива с товарами из корзины
 
 
 @bp.route('/download_zip', methods=['POST'])
-@login_required
+@login_required  # Декоратор для проверки наличия аутентификации
 def download_zip():
     db = get_db()
     cart_items = db.execute(
@@ -145,6 +161,8 @@ def download_zip():
     cache.seek(0)
     return send_file(cache, as_attachment=True, download_name=zip_name)
 
+# Функция для получения пути к файлу товара по его идентификатору
+
 
 def get_PATH_by_item_id(item_id):
     db = get_db()
@@ -158,9 +176,11 @@ def get_PATH_by_item_id(item_id):
     dir_path = os.path.dirname(os.path.realpath(__file__))
     return dir_path+"/static/files/"+file_name['original_file_name']
 
+# Функция для скачивания товара
+
 
 @bp.route('/download/<int:item_id>', methods=['POST'])
-@login_required
+@login_required  # Декоратор для проверки наличия аутентификации
 def download_item(item_id):
     PATH = get_PATH_by_item_id(item_id)
     if PATH:
@@ -168,13 +188,11 @@ def download_item(item_id):
     else:
         flash('Файл не найден.')
 
-    # return redirect(url_for('cart.checkout'))
 
-
+# Функция для отправки товара по электронной почте
 @bp.route('/mail/<int:item_id>', methods=['POST'])
-@login_required
+@login_required  # Декоратор для проверки наличия аутентификации
 def mail_item(item_id):
-    import os
     PATH = get_PATH_by_item_id(item_id)
     db = get_db()
     file_name = db.execute(
--- marketplace/store.py
+++ marketplace/store.py
@@ -11,19 +11,25 @@ from marketplace.db import get_db
 from flask import render_template, flash, redirect, url_for
 from .db import get_item_by_id
 
-
+# Создаем Blueprint с именем 'store'
 bp = Blueprint('store', __name__)
+# Получаем путь к текущей директории
 dir_path = os.path.dirname(os.path.realpath(__file__))
+# Устанавливаем путь к папке с изображениями
 IMG_FOLDER = dir_path + '/static/img'
+# Устанавливаем путь к папке с файлами
 FILE_FOLDER = dir_path + '/static/files'
 
 
 @bp.route('/')
 def index():
+    # Если пользователь не авторизован, перенаправляем на страницу входа
     if not g.user:
         return redirect(url_for('auth.login'))
+    # Получаем доступ к базе данных
     db = get_db()
 
+    # Создаем таблицу item, если она не существует
     db.execute('CREATE TABLE IF NOT EXISTS item (id INTEGER PRIMARY KEY AUTOINCREMENT,'
                'item_name TEXT NOT NULL,'
                'item_description TEXT,'
@@ -33,17 +39,20 @@ def index():
                'secured_name TEXT NOT NULL,'
                'original_file_name TEXT NOT NULL)')
 
+    # Создаем таблицу cart, если она не существует
     db.execute('CREATE TABLE IF NOT EXISTS cart (cart_id INTEGER PRIMARY KEY AUTOINCREMENT,'
                'user_id INTEGER NOT NULL,'
                'item_id INTEGER NOT NULL,'
                'FOREIGN KEY (user_id) REFERENCES user (id),'
                'FOREIGN KEY (item_id) REFERENCES item (id))')
 
+    # Получаем все товары из базы данных
     items = db.execute(
         'SELECT *'
         ' FROM item i'
     ).fetchall()
 
+    # Отображаем главную страницу магазина с товарами
     return render_template('store/index.html', items=items)
 
 
@@ -51,7 +60,9 @@ def index():
 @login_required
 # @admin_only
 def create():
+    # Если метод запроса - POST, значит форма была отправлена
     if request.method == 'POST':
+        # Получаем данные из формы
         data = request.values.to_dict()
         item_name = data["item_name"]
         item_description = data["item_description"]
@@ -60,13 +71,16 @@ def create():
         errors = []
         dataset_author = data["dataset_author"]
 
+        # Проверяем, был ли выбран файл с данными
         if len(item_file.filename) != 0:
             secure_filename(item_file.filename)
             secured_name = uuid.uuid4().hex
             file_name = secured_name + '.'+(item_file.filename.split('.')[-1])
             original_file_name = file_name
 
+            # Сохраняем файл с данными в указанную папку
             item_file.save(os.path.join(FILE_FOLDER, file_name))
+            # Если файл имеет расширение .xlsx, конвертируем его в .csv
             if (item_file.filename.split('.')[-1]) == "xlsx":
                 data = pd.read_excel(os.path.join(FILE_FOLDER, file_name))
                 PATH = os.path.join(FILE_FOLDER, secured_name)
@@ -78,42 +92,51 @@ def create():
             errors.append(error)
             flash(error)
 
+        # Проверяем, было ли указано описание товара
         if not item_description:
             error = 'Описание обязательно'
             errors.append(error)
             flash(error)
+        # Проверяем, было ли выбрано изображение товара
         if not item_image:
             error = 'Изображение обязательно'
             errors.append(error)
             flash(error)
+        # Проверяем, был ли выбран файл с данными
         if not item_file:
             error = 'Набор данных обязателен'
             errors.append(error)
             flash(error)
 
+        # Если было выбрано изображение товара, сохраняем его в указанную папку
         if item_image:
             secure_filename(item_image.filename)
             item_image.save(os.path.join(IMG_FOLDER, item_image.filename))
 
+        # Проверяем, было ли указано название товара
         if not item_name:
             error = 'Заголовок обязателен'
             errors.append(error)
             flash(error)
         else:
             try:
+                # Получаем доступ к базе данных
                 db = get_db()
+                # Выполняем SQL-запрос для добавления нового товара в таблицу item
                 db.execute(
                     'INSERT INTO item (item_name, item_description, item_image, dataset_author, secured_name, file_name, original_file_name)'
                     ' VALUES (?, ?, ?, ?, ?, ?, ?)',
                     (item_name, item_description, item_image.filename,
                      dataset_author, secured_name, file_name, original_file_name)
                 )
+                # Применяем изменения в базе данных
                 db.commit()
 
                 flash(item_name + ' was added to the store', 'success')
             except:
                 return render_template('store/create.html', errors=errors)
 
+    # Отображаем страницу с формой для создания товара
     return render_template('store/create.html')
 
 
@@ -121,16 +144,24 @@ def create():
 @login_required
 @admin_only
 def delete(item_id):
+    # Получаем доступ к базе данных
     db = get_db()
+    # Выполняем SQL-запрос для удаления элемента из таблицы item по заданному id
     db.execute('DELETE FROM item WHERE id = ?', [item_id])
+    # Применяем изменения в базе данных
     db.commit()
+    # Перенаправляем пользователя на главную страницу магазина
     return redirect(url_for('store.index'))
 
 
 @bp.route('/store/item/<int:item_id>')
 def view_item(item_id):
+    # Получаем информацию о товаре по заданному id
     item = get_item_by_id(item_id)
+    # Если товар найден, отображаем страницу с информацией о товаре
     if item:
         return render_template('store/item.html', item=item)
+    # Если товар не найден, выводим сообщение об ошибке
     flash('Item not found')
+    # Перенаправляем пользователя на главную страницу магазина
     return redirect(url_for('store.index'))
