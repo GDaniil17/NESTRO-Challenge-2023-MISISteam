7f5cde105cce4adbdf018e4ed05d88b4da020cd7
GDaniil17 <galimovdv@yandex.ru>
2023-09-25 22:42:45 +0300

    Current version

--- marketplace/__init__.py
+++ marketplace/__init__.py
@@ -5,7 +5,6 @@ from . import db
 from . import auth
 from . import store
 from . import cart
-from flask import send_from_directory
 from flask import render_template
 from werkzeug.utils import secure_filename
 import csv
@@ -15,7 +14,6 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 
 def create_app(test_config=None):
-    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -23,19 +21,15 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    # a simple page that says hello :)
     @app.route('/hello')
     def hello():
         return "Hello World!! 🤡"
@@ -46,10 +40,6 @@ def create_app(test_config=None):
             lines = [tuple(row) for row in reader]
         return (lines[0], lines[1:])
 
-    # headers = ("Rank", "Username", "Categories", "Subscribers", "Country", "Visits", "Likes", "Comments" и "Links")
-    # rows = (('Боба Биба Бобович', '18', 'Male'),
-    #         ('Боба Биба Бобович', '20', 'Male')
-    #         )
 
     @app.route('/table/<path>')
     def table(path):
@@ -98,13 +88,10 @@ def create_app(test_config=None):
     @app.route('/upload_file', methods=['GET', 'POST'])
     def upload_file():
         if request.method == 'POST':
-            # check if the post request has the file part
             if 'file' not in request.files:
                 flash('No file part')
                 return redirect(request.url)
             file = request.files['file']
-            # If the user does not select a file, the browser submits an
-            # empty file without a filename.
             if file.filename == '':
                 flash('No selected file')
                 return redirect(request.url)
@@ -114,7 +101,6 @@ def create_app(test_config=None):
                 return redirect(url_for('upload_file', name=filename))
         return render_template('loader.html')
 
-    # initialize app and blueprints
     db.init_app(app)
     app.register_blueprint(auth.bp)
     app.register_blueprint(store.bp)
--- marketplace/cart.py
+++ marketplace/cart.py
@@ -1,4 +1,4 @@
-import uuid
+import re
 import zipfile
 import shutil
 import tempfile
@@ -10,50 +10,27 @@ import csv
 from marketplace.db import get_db
 from marketplace.auth import login_required
 from flask import Blueprint, g, flash, redirect, url_for
-from flask_mail import Mail
-from flask_mail import Message
 import os
 
 
 bp = Blueprint('cart', __name__)
 
-
-# @bp.route('/add_cart/<int:item_id>', methods=['POST'])
-# @login_required
-# def add_cart(item_id):
-#     db = get_db()
-#     db.execute(
-#         'INSERT INTO cart (user_id, item_id)'
-#         ' VALUES (?, ?)',
-#         (g.user['id'], item_id)
-#     )
-#     db.commit()
-#     print("Item added!")
-#     flash("Item successfully added to cart", 'success')
-#     return redirect(url_for('store.index'))
-
 @bp.route('/add_cart/<int:item_id>', methods=['POST'])
 @login_required
 def add_cart(item_id):
     db = get_db()
-    print("get db!")
-    # TODO
     db.execute(
         'INSERT INTO cart (user_id, item_id) '
-        # f"VALUES ({g.user['id']}, {item_id})"
         ' SELECT ?, ?'
         ' WHERE NOT EXISTS (SELECT 1 FROM cart WHERE item_id = ?)',
         (g.user['id'], item_id, item_id)
     )
     db.commit()
-    print("Item added!")
-    # flash("Item successfully added to cart", 'success')
     return redirect(url_for('store.index'))
 
 
 def read_file(path):
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    print(f"!!!!(((((()))))) {dir_path+'/'+path}")
     with open(dir_path+"/"+path, newline='', encoding='utf-8') as file:
         reader = csv.reader(file, quotechar='"')
         lines = [tuple(row) for row in reader]
@@ -61,7 +38,7 @@ def read_file(path):
 
 
 def table(path):
-    headers, rows = read_file(path)  # 'youtubers_df.csv')
+    headers, rows = read_file(path)
     return render_template('columns.html', headers=headers, rows=rows)
 
 
@@ -69,16 +46,13 @@ def table(path):
 @login_required
 def preview(item_id):
     db = get_db()
-    print(f"get db!!!! {item_id}")
     file_name = db.execute(
         'SELECT file_name, id FROM item '
         f'WHERE id = {item_id}'
     ).fetchone()
 
     db.commit()
-    print("Item selected!")
     path = f"static/files/{file_name['file_name']}"
-    # flash("Item successfully added to cart", 'success')
     return table(path)
 
 
@@ -86,8 +60,6 @@ def preview(item_id):
 @login_required
 def checkout():
     db = get_db()
-    all = db.execute('Select * from cart').fetchall()
-    print(all)
     cart_items = db.execute(
         'SELECT cart_id, i.item_name, i.dataset_author, i.item_description, i.item_image FROM cart c'
         ' INNER JOIN item i ON c.item_id = i.id'
@@ -100,7 +72,7 @@ def checkout():
 @bp.route('/tag/<item_dataset_author>', methods=['GET'])
 @login_required
 def tag(item_dataset_author):
-    import re
+    title = f"{item_dataset_author.strip()}"
     item_dataset_author = item_dataset_author.strip().lower()
     item_dataset_author = item_dataset_author.replace(" ", "*")
     db = get_db()
@@ -112,33 +84,25 @@ def tag(item_dataset_author):
 
     new_items = []
     for i in items:
-        print(item_dataset_author, i['dataset_author'], re.search(item_dataset_author, i['dataset_author']))
         if re.search(item_dataset_author, i['dataset_author'].lower()) is not None:
             new_items.append(i)
 
-    return render_template('store/index.html', items=new_items)
+    return render_template('store/index.html', items=new_items, title=title)
 
 
 @bp.route('/delete/<cart_item_id>', methods=['POST'])
 @login_required
 def delete_item(cart_item_id):
-    print("HERE")
     db = get_db()
-    print(cart_item_id)
     db.execute('DELETE FROM cart WHERE cart_id = ?', [cart_item_id])
     db.commit()
     return redirect(url_for('cart.checkout'))
 
 
 def create_zip_archive(file_list, archive_name):
-    print(archive_name)
     with zipfile.ZipFile(archive_name, 'w') as zip_file:
         for file_path in file_list:
-            # Получаем полный путь к файлу
             full_path = os.path.join(os.getcwd(), 'marketplace', 'static', 'files', file_path)
-
-            print(f"!!!!!!!!*** {file_path} {os.path.exists(full_path)}")
-            # Добавляем файл в архив
             zip_file.write(full_path, file_path)
 
 
@@ -153,9 +117,8 @@ def download_zip():
         [g.user['id']]
     ).fetchall()
     db.commit()
-    zip_name = '1987f5f539e3406a979d97338efade39.zip'# uuid.uuid4().hex+".zip"
+    zip_name = 'ziped_data.zip'# uuid.uuid4().hex+".zip"
     files = [item['original_file_name'] for item in cart_items]
-    print(f"!!!!!!!!!!!!!!!!!! {files}")
     create_zip_archive(files, zip_name)
     path = os.path.join(
         os.getcwd(),
@@ -176,11 +139,8 @@ def get_PATH_by_item_id(item_id):
         'SELECT original_file_name, id FROM item '
         f'WHERE id = {item_id}'
     ).fetchone()
-    # lsx csv
     db.commit()
-    import os
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    print(dir_path+"/static/files/"+file_name['original_file_name'])
     return dir_path+"/static/files/"+file_name['original_file_name']
 
 
@@ -188,20 +148,15 @@ def get_PATH_by_item_id(item_id):
 @login_required
 def download_item(item_id):
     PATH = get_PATH_by_item_id(item_id)
-    print("!!!!(((((()))))) ", PATH)
     if PATH:
         return send_file(PATH, as_attachment=True)
     else:
         flash('Файл не найден.')
 
-    # return redirect(url_for('cart.checkout'))
-
 
 @bp.route('/mail/<int:item_id>', methods=['POST'])
 @login_required
 def mail_item(item_id):
-    import os
-    print("HERE")
     PATH = get_PATH_by_item_id(item_id)
     db = get_db()
     file_name = db.execute(
@@ -209,7 +164,6 @@ def mail_item(item_id):
         f'WHERE id = {item_id}'
     ).fetchone()
 
-    print("!!!!(****** ", PATH, os.path.isfile(PATH))
     email_address = ''
     subject = 'Датасет - '+str(file_name['item_name'])
     body = 'Описание датасета - ' + str(file_name['item_description'])
